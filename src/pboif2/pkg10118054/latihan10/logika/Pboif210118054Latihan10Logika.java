9fd22cd1325ff66962e6bcb57d65267d4dc4992b
farhan3211 <55830838+farhan3211@users.noreply.github.com>
2019-09-26 16:23:20 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118054/latihan10/logika/Pboif210118054Latihan10Logika.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118054.latihan10.logika;
+
+/**
+ *
+ * @author ASUS
+ * nama  :Farhan Milardi
+ * kelas :IF-2
+ * nim   :10118054
+ */
+public class Pboif210118054Latihan10Logika {
+
+    /**
+     * @param args the command line arguments
+     * int umur = 19;
+        boolean dewasa;
+        dewasa = (umur >= 17);
+        System.out.println("Status Dewasa : " + dewasa);
+
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
